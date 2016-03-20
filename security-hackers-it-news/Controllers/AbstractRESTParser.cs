c2de3997e120cb229b84d5fdeaf0e9c05ff515f9
pete314 <webpmaster@gmail.com>
2016-03-20 12:35:50 +0000

    Initial commit

---
+++ security-hackers-it-news/Controllers/AbstractRESTParser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+
+namespace security_hackers_it_news.Controllers
+{
+    /// <summary>
+    /// Abstract class holding the reusable methods
+    /// </summary>
+    class AbstractRESTParser
+    {
+        object jsonString;
+        public object JsonString { get { return jsonString; } private set { JsonString = value; } }
+
+        private HttpClient client;
+        /// <summary>
+        /// The Http client to try parse rest api 
+        /// </summary>
+        public HttpClient Client
+        {
+            get { return client; }
+            set { client = value; }
+        }
+
+        private string currentURL;
+ 
+        public string CurrentUrl
+        {
+            get { return currentURL; }
+            set { currentURL = value; }
+        }
+
+        private HttpResponseMessage httpResponse;
+
+        public HttpResponseMessage HttpRpMsg
+        {
+            get { return httpResponse; }
+            private set { httpResponse = value; }
+        }
+
+        public AbstractRESTParser()
+        {
+            client = new HttpClient();
+        }
+
+        protected async void tryParseUrl(string url) {
+            currentURL = url;
+            try {
+                httpResponse = await client.GetAsync(new Uri(currentURL));
+                if (httpResponse.StatusCode == System.Net.HttpStatusCode.OK) {
+                    jsonString = await httpResponse.Content.ReadAsStringAsync();
+                }
+            }
+            catch{
+                //there was no internet 
+            }
+        }
+
+        protected object getResultAs(string type) {
+            switch (type) {
+                case "JsonArray":
+                    return JsonValue.Parse((string)jsonString).GetArray();
+                default:
+                    return jsonString;
+            }
+        }
+    }
+}
---
+++ security-hackers-it-news/Models/HNewsItemModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace security_hackers_it_news.Models
+{
+    /// <summary>
+    /// Item model for hackers news entries
+    /// </summary>
+    class HNewsItemModel
+    {
+        private int id = 0;//Unique id
+        private bool deleted = false;//is deleted
+        private string type = "story";//story/job/comment/poll/pollopt
+        private string by = "John Doe";//publisher name
+        private int time = 1;//Unix timestamp
+        private string text = "";//the body of the item/comment
+        private bool dead = false;//item is dead
+        private int parent = 0; //comment parent
+        private List<int> kids;
+        private string url; //the url for the story
+        private string score;
+        private string title; //if job
+        
+    }
+}
